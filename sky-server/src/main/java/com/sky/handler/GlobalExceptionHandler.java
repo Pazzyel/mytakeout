0ee6f0c2af46bbc71ca5d15af14b116446c47291
Pazzyel <huangjin6380@gmail.com>
2025-06-25 22:05:09 +0800

    添加了针对用户名已存在的SQL异常处理

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +25,26 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 处理重复用户名异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        //异常的信息为:Duplicate entry '小海' for key 'employee.idx_username'
+        String message = ex.getMessage();
+        if(message.contains("Duplicate entry")){
+            //说明是存在重复的key类型的异常
+            String username = message.split(" ")[2];
+            String errmsg = username + MessageConstant.ALREADY_EXISTS;
+            return Result.error(errmsg);
+        } else {
+            //不是重复用户名错误，返回未知错误
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
     }
 
 }
