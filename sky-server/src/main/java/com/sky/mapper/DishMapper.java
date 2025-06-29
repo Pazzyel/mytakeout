99292f38468070579621f3c6aa990d347e5e0bf4
Pazzyel <huangjin6380@gmail.com>
2025-06-29 21:35:04 +0800

    添加了新增菜品的功能

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,6 +1,11 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
@@ -12,4 +17,14 @@ public interface DishMapper {
      */
     @Select("SELECT COUNT(*) FROM dish WHERE category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
+
+    /**
+     * 插入菜品数据
+     * @param dish
+     */
+    @AutoFill(OperationType.INSERT)
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    @Insert("INSERT INTO dish ( name, category_id, price, image, description, status, create_time, update_time, create_user, update_user)" +
+            "VALUES (#{name}, #{categoryId}, #{price}, #{image}, #{description}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
+    void insert(Dish dish);
 }
---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,35 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DishServiceImpl implements DishService {
+    @Autowired
+    DishMapper dishMapper;
+    /**
+     * 新增菜品
+     * @param dishDTO
+     */
+    @Override
+    public void saveWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO, dish);
+
+        dishMapper.insert(dish);
+        Long dishId = dish.getId();//主键回填
+
+        List<DishFlavor> dishFlavorList = dishDTO.getFlavors();
+        if (dishFlavorList != null && dishFlavorList.size() > 0) {
+            dishFlavorList.forEach(dishFlavor -> {dishFlavor.setDishId(dishId);});
+        }
+    }
+}
