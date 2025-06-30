09c823ecf1524590090d539d74432feb23ca95ac
Pazzyel <huangjin6380@gmail.com>
2025-06-30 23:41:30 +0800

    修改了上传图片的接口bug，新增访问图片资源的接口

--- sky-common/src/main/java/com/sky/constant/UploadConstant.java
+++ sky-common/src/main/java/com/sky/constant/UploadConstant.java
@@ -4,4 +4,5 @@ import java.io.File;
 
 public class UploadConstant {
     public static final String UPLOAD_PATH = "D:\\Coding\\Takeout\\uploads";
+    public static final String REQUEST_PATH = "http://127.0.0.1:8083/uploads/";
 }
--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -6,16 +6,15 @@ import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.nio.file.Files;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/admin/common")
 @Slf4j
 @Api(tags = "通用接口")
 public class CommonController {
@@ -25,7 +24,7 @@ public class CommonController {
      * @param file
      * @return
      */
-    @PostMapping("/upload")
+    @PostMapping("/admin/common/upload")
     @ApiOperation("文件上传接口")
     public Result upload(MultipartFile file) {
         log.info("文件上传: {}", file);
@@ -38,11 +37,30 @@ public class CommonController {
             File newfile = new File(UploadConstant.UPLOAD_PATH, fileName);
             log.info("正在上传文件");
             file.transferTo(newfile);//保存到指定位置
-            return Result.success(UploadConstant.UPLOAD_PATH + "\\" + fileName);
+            return Result.success(UploadConstant.REQUEST_PATH + "/" + fileName);
         } catch (Exception e) {
             e.printStackTrace();
             log.error("文件上传失败: {}",e.getMessage());
         }
         return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+
+    @GetMapping("/uploads/{filename}")
+    public void getFile(@PathVariable String filename, HttpServletResponse response) {
+        try {
+            File file = new File(UploadConstant.UPLOAD_PATH + "/" + filename);
+            String contentType = Files.probeContentType(file.toPath());
+            if (contentType == null) {
+                contentType = "application/octet-stream";
+            }
+            if (file.exists()) {
+                response.setContentType(contentType);
+                Files.copy(file.toPath(), response.getOutputStream());
+            } else {
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
